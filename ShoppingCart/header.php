e419b255d25381d6da4633deff6019e75af0e8d5
Humphrey Ibekwe <102169560+HumphTriplePoleESamples@users.noreply.github.com>
2024-06-19 16:33:10 +0100

    Add files via upload

---
+++ ShoppingCart/header.php
@@ -0,0 +1,18 @@
+<!-- header -->
+
+<header class="header">
+<div class="header_body">
+    <a href="index.php" class="logo">333PoleSoftTech</a>
+    <nav class="navbar">
+        <a href="">Add Product</a>
+        <a href="">View Product</a>
+        <a href="">Shopit</a>
+    </nav>
+    
+    <!-- Shopping Cart Icon -->
+    <a href="" class="cart"><i class="fa-solid fa-cart-shopping"></i><span><sup>4</sup></span></a>
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+</div>
+
+</header>
