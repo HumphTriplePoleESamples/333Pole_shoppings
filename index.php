5a0c1186f97d38d8f93605ac7269922758275e9a
Humphrey IBEKWE <humphreyoibekwe@gmail.com>
2024-06-19 15:11:37 +0100

    333Pole_shopping

---
+++ index.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fast Food Web Design</title>
+    <link rel="stylesheet" href="style.css"/>
+
+</head>
+<body>
+    <header>
+        <div class="navbar">
+            <a href="" class="logo">Fast <span>Food</span></a>
+            <ul class="lines"></ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#menu">Menu</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </div>
+    </header>
+
+    <main>
+        <section id="home" class="home">
+            <div class="container">
+                <div class="left">
+                    <h2>
+                        <span>quality</span><br>you can taste
+                    </h2>
+                    <p>Order for your delicious, tantalizing, sumptious meals 
+                        and nutritious drinks. Have a bite of this well 
+                        prepared chicken. A taste will 
+                        definitely convince you. Enjoy every bit of it. 
+                        Many thanks for your patronage
+                    </p>
+                        <a href="menu">veiw menu</a>
+                </div>
+                <div class="right">
+                    <div class="img-container">
+                        <img src="img/cook1.webp" alt="" />
+                    </div>
+                </div>
+                <div class="gallery">
+                    <div class="img-wrapper">
+                        <img src="img/chicken1.jpg" alt="" />
+                    </div>
+                    <div class="img-wrapper">
+                        <img src="img/wine2.jpg" alt="" />
+                    </div>
+                    <div class="img-wrapper">
+                        <img src="img/tombo1.jpg" alt="" />
+                    </div>
+                    <div class="img-wrapper">
+                        <img src="img/zobo.jpg" alt="" />
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="services" class="services">
+            <div class="img-container">
+                <img src="img/seafood.jpeg" alt="" />
+            </div>
+            <div class="container">
+                <div class="list">
+                    <div class="card">
+                        <div class="title">
+                            <div class="number">01</div>
+                            Title
+                        </div>
+                        <p>
+                        Enjoy your tantalizing and delicious sea food.
+                        You will definitly come again after having your good time. 
+                        Many thanks for your patronage  
+                        </p>
+                        <a href="a">learn more</a>
+
+                        <div class="card">
+                        <div class="title">
+                            <div class="number">02</div>
+                            Title
+                        </div>
+                        <p>
+                        Enjoy your tantalizing and delicious hot chicken.
+                        You will definitly come again after having your good time. 
+                        Many thanks for your patronage  
+                        </p>
+                        <a href="a">learn more</a>
+
+                        <div class="card">
+                        <div class="title">
+                            <div class="number">03</div>
+                            Title
+                        </div>
+                        <p>
+                        Enjoy your tantalizing and delicious hot chicken.
+                        You will definitly come again after having your good time. 
+                        Many thanks for your patronage  
+                        </p>
+                        <a href="a">learn more</a>
+
+                        <div class="card">
+                        <div class="title">
+                            <div class="number">04</div>
+                            Title
+                        </div>
+                        <p>
+                        Enjoy your tantalizing and delicious hot chicken.
+                        You will definitly come again after having your good time. 
+                        Many thanks for your patronage 
+                        </p>
+                        <a href="a">learn more</a>
+
+                        <div class="card">
+                        <div class="title">
+                            <div class="number">05</div>
+                            Title
+                        </div>
+                        <p>
+                        Enjoy your tantalizing and delicious hot chicken.
+                        You will definitly come again after having your good time. 
+                        Many thanks for your patronage  
+                        </p>
+                        <a href="a">learn more</a>
+
+                        <div class="card">
+                        <div class="title">
+                            <div class="number">06</div>
+                            Title
+                        </div>
+                        <p>
+                        Enjoy your tantalizing and delicious hot chicken.
+                        You will definitly come again after having your good time. 
+                        Many thanks for your patronage 
+                        </p>
+                        <a href="a">learn more</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="menu" class="menu">
+            <div class="container">
+               <h2>Menu</h2>
+               <p>
+                Please try enjoy yourself with tasty & 
+                healthy palmy, fine wine, Bear & zobo.
+                Many thanks for your patronage
+               </p>
+            </div>
+            <div class="list">
+                <div class="card card1">
+                    <div class="info">
+                        <h2>Pizza</h2>
+                        <div class="price">$3.99</div>
+                        <button>add to cart</button>
+                    </div>
+                    <div class="card card2">
+                    <div class="info">
+                        <h2>Sweat Rolls</h2>
+                        <div class="price">$4.50</div>
+                        <button>add to cart</button>
+                    </div>
+                    <div class="card card3">
+                    <div class="info">
+                        <h2>Fish Salad</h2>
+                        <div class="price">$4.65</div>
+                        <button>add to cart</button>
+                    </div>
+                    <div class="card card4">
+                    <div class="info">
+                        <h2>Vege Salad</h2>
+                        <div class="price">$5.48</div>
+                        <button>add to cart</button>
+                    </div>
+                    <div class="card card5">
+                    <div class="info">
+                        <h2>Chicken</h2>
+                        <div class="price">$4.99</div>
+                        <button>add to cart</button>
+                    </div>
+                    <div class="card card6">
+                    <div class="info">
+                        <h2>Sea Food</h2>
+                        <div class="price">$2.58</div>
+                        <button>add to cart</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+    
+</body>
+</html>
